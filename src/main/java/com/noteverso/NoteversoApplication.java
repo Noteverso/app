84c34ef81e58031c65aa4f21caa97cb92b766d51
byodian <byj6696@gmail.com>
2023-05-18 14:35:53 +0800

    first commit

---
+++ src/main/java/com/noteverso/NoteversoApplication.java
@@ -0,0 +1,13 @@
+package com.noteverso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteversoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteversoApplication.class, args);
+	}
+
+}
